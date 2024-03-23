afd1e41db07a59e9b3e9a0ed88e5dd35d73f0b74
Ken <kennethwarui35@gmail.com>
2024-03-23 12:14:08 +0300

    succesful CRUD operation

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	// "fmt"
-	// "strings"
-	// "html/template"
-	// "net/http"
+  "strings"
 	"html/template"
-
+	"net/http"
+	"fmt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -25,12 +23,46 @@ type TodoPageData struct {
 
 
 func main(){
-    tmpl :=
-    template.Must(template.ParseFiles("template/index.html"))
+    tmpl := template.Must(template.ParseFiles("template/index.html"))
     db, err := gorm.Open(sqlite.Open("test.db"),
-&gorm.Config{})
+    &gorm.Config{})
     if err != nil{
         panic("failed to connect to database")
     }
     db.AutoMigrate(&Todo{})
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request)  {
+  if (r.Method == "POST") {
+// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+    if err := r.ParseForm(); err != nil {
+      fmt.Fprintf(w, "ParseForm() err: %v", err)
+      return
+    }
+    todo := r.FormValue("todo")
+    db.Create(&Todo{Title: todo, Done: false})
+  }
+  //Request not POST 
+    var todos []Todo
+    db.Find(&todos)
+    data := TodoPageData{
+    PageTitle: "My TODO list",
+    Todo: todos,
+    }
+    tmpl.Execute(w, data)
+  })
+  http.HandleFunc("/done/", func(w http.ResponseWriter, r *http.Request) {
+    id := strings.TrimPrefix(r.URL.Path, "/done/")
+    var todo Todo
+    db.First(&todo, id)
+    todo.Done = true
+    db.Save(&todo)
+    http.Redirect(w, r, "/", http.StatusSeeOther)
+  })
+  http.HandleFunc("/delete/",  func(w http.ResponseWriter, r *http.Request) {
+    id := strings.TrimPrefix(r.URL.Path, "/delete/")
+    db.Delete(&Todo{}, id) 
+    http.Redirect(w, r, "/", http.StatusSeeOther)
+  })
+
+  http.ListenAndServe(":8090", nil)
 }
+
